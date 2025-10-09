c8cb876f5e0b451787b1919ba5433d75684b9f5c
Arush Bhimwal <arushbhi2007@gmail.com>
2025-10-09 11:04:12 +0530

    modified:   007_password_generator.py

--- 007_password_generator.py
+++ 007_password_generator.py
@@ -7,9 +7,25 @@ upper_case = list(string.ascii_uppercase)
 numbers = list(string.digits)
 symbols = list(string.punctuation)
 
+final_list = [
+
+]
+
+password_list = [
+
+]
+
 no_of_selection = 0
 
-print(lower_case, upper_case, numbers, symbols)
+# print(lower_case, upper_case, numbers, symbols)
+
+print("========== Password Generator (12-32 characters) ==========")
+print("")
+print('''Security Levels:
+  12-15 chars: Good security
+  16-24 chars: Strong security  
+  25-32 chars: Maximum security''')
+print("")
 
 while True:
     try:
@@ -17,10 +33,14 @@ while True:
 
         if password_length >= 12 and password_length <= 32:
             break
+
         else:
             print("Invalid input, Pls enter a valid password length (12-32)")
+        
     except ValueError:
         print("Invalid input, Pls enter a valid password length (12-32)")
+
+print("Character types to include: ")
 
 while True:
     try:
@@ -90,25 +110,29 @@ while True:
     except ValueError:
         print("Invalid input, Pls enter a valid input (y-n)")
 
-print(password_length, lower_case_complexity, upper_case_complexity, numbers_complexity, symbols_complexity, no_of_selection)
-
-final_list = [
-
-]
+# print(password_length, lower_case_complexity, upper_case_complexity, numbers_complexity, symbols_complexity, no_of_selection)
 
 if lower_case_complexity == "y":
     final_list.extend(lower_case)
+    password_list.append(random.choice(lower_case))
 
 if upper_case_complexity == "y":
     final_list.extend(upper_case)
+    password_list.append(random.choice(upper_case))
 
 if numbers_complexity == "y":
     final_list.extend(numbers)
+    password_list.append(random.choice(numbers))
 
 if symbols_complexity == "y":
     final_list.extend(symbols)
+    password_list.append(random.choice(symbols))
 
-print(final_list)
+if not final_list:
+    print("No character categories selected, Can't generate password.")
+    exit()
+
+# print(final_list)
 
 while True:
     try:
@@ -139,12 +163,22 @@ if exclude_similars == "y":
         while ch in symbols:
             symbols.remove(ch)
             
-
-print(final_list)
-
-password_list = [
-
-]
+# print(final_list)
 
 for i in range(password_length - no_of_selection):
-    pass
+    password_list.append(random.choice(final_list))
+
+# print(password_list)
+
+random.shuffle(password_list)
+
+shuffled_password = "".join (password_list)
+
+print("")
+print(f"Generated Password: {shuffled_password}")
+print(f"Length: {password_length} characters")
+
+pyperclip.copy(shuffled_password)
+
+print("Password copied to clipboard!")
+
